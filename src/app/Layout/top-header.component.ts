220f0bead7eff802a2200ab55d0e9ac354a974a3
hardikidea <hardikidea@gmail.com>
2018-11-06 18:23:01 +0530

    Reset Theme

---
+++ src/app/Layout/top-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-header',
+  templateUrl: './top-header.component.html',
+  styles: []
+})
+export class TopHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopHeaderComponent } from './Layout/top-header.component';
+import { LeftSidebarComponent } from './Layout/left-sidebar.component';
+import { BreadcrumbComponent } from './Layout/breadcrumb.component';
+import { DashboardComponent } from './Layout/dashboard.component';
+import { FooterComponent } from './Layout/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopHeaderComponent,
+    LeftSidebarComponent,
+    BreadcrumbComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
